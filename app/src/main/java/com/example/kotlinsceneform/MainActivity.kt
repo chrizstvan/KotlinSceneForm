ab09e7223add57d173ba9db61e8f03702ea077fc
Christian Stevanus <chris.stev30@gmail.com>
2019-03-07 23:58:36 +0700

    Add tap gesture to show 3D object

--- app/src/main/java/com/example/kotlinsceneform/MainActivity.kt
+++ app/src/main/java/com/example/kotlinsceneform/MainActivity.kt
@@ -4,8 +4,11 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.rendering.Color
 import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
@@ -21,6 +24,8 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var hipoReadable: ModelRenderable
 
     internal var selected = 1 // Default 1 is choose the bear
+
+    lateinit var arFragment: ArFragment
 
     override fun onClick(view: View?) {
 
@@ -85,6 +90,28 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setupClickListener()
         setup3DModel()
 
+        arFragment = supportFragmentManager.findFragmentById(R.id.scene_form_fragment) as ArFragment
+
+        arFragment.setOnTapArPlaneListener { hitResult, plane, motionEvent ->
+            val anchor = hitResult.createAnchor()
+            val anchorNode = AnchorNode(anchor)
+
+            anchorNode.setParent(arFragment.arSceneView.scene)
+
+            CreateModel(anchorNode, selected)
+        }
+
+    }
+
+    private fun CreateModel(anchorNode: AnchorNode, selected: Int) {
+
+        if (selected == 1)
+        {
+            val bear = TransformableNode(arFragment.transformationSystem)
+            bear.setParent(anchorNode)
+            bear.renderable = bearReadable
+            bear.select()
+        }
     }
 
     private fun setup3DModel() {
